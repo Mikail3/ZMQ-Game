b98d8948c58d5c58a6dcc49369cf685bbde60d20
Mikail3 <43419941+Mikail3@users.noreply.github.com>
2020-05-19 13:30:47 +0200

    Update Client.c

--- Client.c
+++ Client.c
@@ -11,14 +11,18 @@ int main( int argc, char * argv[] )
     void *publisher = zmq_socket(context, ZMQ_PUSH);
     void *subscriber = zmq_socket (context, ZMQ_SUB);
 
-    //connect
+
     int rd = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
     int rc = zmq_connect( subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
     sleep (3);
 
 
-    const char *filter = (argc > 1)? argv [1]: "example>task!>Mikail";
-    const char *filter2 = (argc > 1)? argv [1]: "example>answer!>Mikail";
+    const char *joinfilter = (argc > 1)? argv [1]: "BullsCows>join!>";
+    const char *gamefilter = (argc > 1)? argv [1]: "BullsCows>game!>";
+
+    const char *jointopic = (argc > 1)? argv [1]: "BullsCows>join?>";
+    const char *gametopic = (argc > 1)? argv [1]: "BullsCows>game?>";
+
 
 
     for(int i=0; i < 2; i++)
@@ -28,25 +32,15 @@ int main( int argc, char * argv[] )
 
         zmq_msg_t message;
         rc = zmq_msg_init (&message);
-        switch(i)
-        {
-            case 0:
-                rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filter, strlen (filter));
-                //send and receive
-                rd = zmq_send(publisher, "example>task?>Mikail Cinar>", 29, 0); printf("message send\n");
-                assert (rd == 29);
-                break;
-            case 1:
 
-                rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filter2, strlen (filter2));
+                rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, joinfilter, strlen (joinfilter));
                 //send and receive
-                rd = zmq_send(publisher, "example>answer?>Mikail Cinar >CORONA-FREE-CHANNEL>4cb9aedf2d6c0a521be6af4d6d097eb78f868c83>*HATSJU*>", 101, 0);
-                assert (rd ==101);
-                printf("answer send\n");
-                break;
-        }
+                rd = zmq_send(publisher, jointopic, strlen(jointopic), 0); printf("message send\n");
+
         printf("Waiting for reply\n");
+
         rc = zmq_recv (subscriber, buf, 256, 0); assert(rc != -1);
+
         int size = zmq_msg_size (&message);
         char *string = malloc (size + 1);
         memcpy (string, zmq_msg_data (&message), size);
@@ -55,7 +49,7 @@ int main( int argc, char * argv[] )
         printf("Message received: %s\n\n",buf);
         sleep(1);
     }
-    //free (string);
+
     zmq_close(publisher);
     zmq_close( subscriber );
     zmq_ctx_shutdown( context ); //optional for cleaning lady order (get ready you l*zy f*ck)
@@ -63,7 +57,6 @@ int main( int argc, char * argv[] )
     printf("KTNXBYE!\n");
     return 0;
 }
-
 
 /*
 
