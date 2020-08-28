4d7bc7772acdf67153886b2e655ba049bf07a5ed
Mikail Cinar <43419941+Mikail3@users.noreply.github.com>
2020-08-28 11:30:16 +0200

    Add files via upload

--- Client.c
+++ Client.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
 
 int main ( int argc, char * argv [] )
@@ -15,108 +16,67 @@ int main ( int argc, char * argv [] )
     void *receiver = zmq_socket (context, ZMQ_SUB); ///Benternet pulls , u SUB  ,important voor receive
 
 
-    int rs = zmq_connect (sender, "tcp://benternet.pxl-ea-ict.be:24041");
-    int rr = zmq_connect (receiver, "tcp://benternet.pxl-ea-ict.be:24042"); ///important voor receive
-    ///int rc = zmq_setsockopt(receiver , ZMQ_SUBSCRIBE , "example>task?>Mikail>" , 21); ///important , op wat je wilt ontvangen, decides what u receive , BENTERNET SENDS
+    int rs = zmq_connect (sender,"tcp://benternet.pxl-ea-ict.be:24041"); ///"tcp://benternet.pxl-ea-ict.be:24041");
+    int rr = zmq_connect (receiver, "tcp://benternet.pxl-ea-ict.be:24042");///"tcp://benternet.pxl-ea-ict.be:24042"); ///important voor receive
+    ///int rc = zmq_setsockopt(receiver , ZMQ_SUBSCRIBE , "BC!>Joined!" , 21); ///important , op wat je wilt ontvangen, decides what u receive , BENTERNET SENDS
     ///int rc = zmq_setsockopt(receiver , ZMQ_SUBSCRIBE , "example>answer!>Mikail>" , 21);
 
-     ///Making filter
+     const char *berichtJoin = (argc > 1)? argv [1]: "BC>join?>"; ///bericht
+     const char *filterJoin = (argc > 1)? argv [1]: "BC>join!>"; ///filter
+
+       assert (rs == 0);
+       printf("Client started\n");
+
+       sleep(1); ///Bcz send and receive too quick
+      /// rs = zmq_send (sender , "example>task?>Mikail>" ,  21 , 0);
+       char buf [256];
+       int guess [256];
+       char textJoin [20];
+       char textgame [20];
+       char filterPlayer[50];
+       char name [20];
+
+       printf("Enter your name: ");
+       scanf("%s", name);
+
+           strcpy(textJoin, berichtJoin);
+           strcat(textJoin, name);
+           strcat(textJoin, guess);
+           strcat(textJoin, ">");
+
+        do
+       {
+
+           rs = zmq_send(sender, textJoin, strlen(textJoin), 0);
+
+           buf[rs] = '\0';
+           printf("Your input: %d\n\n" ,guess);
+            buf[rs] = '\0';
+           scanf("%d", guess);
+            buf[rs] = '\0';
+           rs = zmq_send (sender , guess ,  256 , 0);
+            buf[rs] = '\0';
+
+            rr = zmq_setsockopt (sender, ZMQ_SUBSCRIBE, filterJoin, strlen (filterJoin));
+                rr = zmq_recv (sender, buf, 256, 0);
+       } while (1); {
 
 
-    assert (rs == 0);
-    printf("Bart saves us\n");
-
-    sleep(1); ///Bcz send and receive too quick
-   /// rs = zmq_send (sender , "example>task?>Mikail>" ,  21 , 0);
-    rs = zmq_send (sender , "example>answer?>Mikail>" ,  21 , 0);
-    printf("Send bytes = %d", rs );
-    char buf[256];
-
-    while (1) {
 
 
-            rr = zmq_recv (receiver, buf ,256 , 0 ); ///receiving message option 1
-            buf[rr] = '\0'; ///String stop
-            printf("Received: %s" , buf);
+               rr = zmq_recv (receiver, buf ,256 , 0 ); ///receiving message option 1
+               buf[rr] = '\0'; ///String stop
+               printf("Received: %s" , buf);
 
-//            zmq_msg_t msg; ///receiving message option 2
-//            int rr = zmq_msg_init (&msg);
-//            assert (rr == 0);
-//            rr = zmq_msg_recv (&msg, receiver, 0); ///important voor receive
-//            zmq_msg_close (&msg);
-    }
-
-
-        zmq_close(receiver);
-        zmq_ctx_destroy(context);
-        return 0;
-}
+   //            zmq_msg_t msg; ///receiving message option 2
+   //            int rr = zmq_msg_init (&msg);
+   //            assert (rr == 0);
+   //            rr = zmq_msg_recv (&msg, receiver, 0); ///important voor receive
+   //            zmq_msg_close (&msg);
+       }
 
 
-/*#include <stdio.h>
-#include <zmq.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <assert.h>
-
-int main( int argc, char * argv[] )
-{
-        void *context = zmq_ctx_new();
-        void *publisher = zmq_socket(context, ZMQ_PUSH);
-        void *subscriber = zmq_socket (context, ZMQ_SUB);
-
-         ///Hello world 4 ZMQ
-        int rd = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
-        int rc = zmq_connect( subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
-        sleep (3);
-
-        //filter instellen
-        const char *filter = (argc > 1)? argv [1]: "example>task!>Mikail";
-        const char *filter2 = (argc > 1)? argv [1]: "example>answer!>Mikail";
-
-    //luisteren naar reactie en verzenden
-        for(int i=0; i < 2; i++)
-        {
-                char buf [256];
-                buf[0] ='\0';
-
-                zmq_msg_t message;
-            rc = zmq_msg_init (&message);
-                switch(i)
-                {
-                        case 0:
-                                rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filter, strlen (filter));
-                                //send and receive
-                                rd = zmq_send(publisher, "example>task?>Mikail>", 29, 0); printf("message send\n");
-                                assert (rd == 29);
-                                break;
-                        case 1:
-
-                                rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filter2, strlen (filter2));
-                                //send and receive
-                                rd = zmq_send(publisher, "example>answer?>Mikail>CORONA-FREE-CHANNEL>e370b8e39ff4609ea22243f5fe71f9def3e14e86>*HATSJU*>", 101, 0);
-                                assert (rd ==101);
-                                printf("answer send\n");
-                                break;
-                }
-                printf("Waiting for reply\n");
-                rc = zmq_recv (subscriber, buf, 256, 0); assert(rc != -1);
-        int size = zmq_msg_size (&message);
-        char *string = malloc (size + 1);
-        memcpy (string, zmq_msg_data (&message), size);
-        zmq_msg_close (&message);
-        string [size] = 0;
-        printf("Message received: %s\n\n",buf);
-                sleep(1);
-        }
-        //free (string);
-        zmq_close(publisher);
-        zmq_close( subscriber );
-        zmq_ctx_shutdown( context ); //optional for cleaning lady order (get ready you l*zy f*ck)
-        zmq_ctx_term( context ); //cleaning lady goes to work
-        printf("KTNXBYE!\n");
-        return 0;
-}
-
-*/
+           zmq_close(receiver);
+           zmq_ctx_destroy(context);
+           return 0;
+   }
---
+++ Server.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <assert.h>
+#include <zmq.h>
+#include <unistd.h>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+void SplitNummer(int number, int array[], int size)
+{
+    for (; number != 0; number /= 10)
+        array[--size] = number%10;
+}
+int main()
+{
+    void *context = zmq_ctx_new ();
+    void *sender = zmq_socket (context, ZMQ_PUSH); ///If u want to send some , send here
+    void *receiver = zmq_socket (context, ZMQ_SUB); ///Benternet pulls , u SUB  ,important voor receive
+
+    int rs = zmq_connect (sender,"tcp://benternet.pxl-ea-ict.be:24041"); ///"tcp://benternet.pxl-ea-ict.be:24041");
+    int rr = zmq_connect (receiver, "tcp://benternet.pxl-ea-ict.be:24042");///"tcp://benternet.pxl-ea-ict.be:24042"); ///important voor receive
+    int rc = zmq_setsockopt(receiver , ZMQ_SUBSCRIBE , "BC?>" , 4);  ///important , op wat je wilt ontvangen, decides what u receive , BENTERNET SENDS
+    int rh = zmq_setsockopt(receiver , ZMQ_SUBSCRIBE , "hint>" , 5);
+
+    assert (rs == 0);
+    printf("Server started\n");
+    sleep(1); ///Bcz send and receive too quick
+
+    while(1)
+    {
+    //infinite while zetten
+    rs = zmq_send (sender , "BullsCows Started" ,  17 , 0);
+    srand(time(NULL));
+    rs = zmq_send (sender , "---Welcome to BULLS & COWS!---" ,  30 , 0);
+
+    int Guesses[256]; /// guesses
+    int size = 0;
+    char buf[256];
+    char input[256];
+    char hint[10];
+
+    /// randomizer
+    const short nummer = 4; ///determining nummer voor grootte van gewenste input
+    std :: string result = "";
+
+    for (int i = 0; i < nummer; i++) ///loop randomizer
+    {
+
+           result.push_back(rand()%10 + '0'); ///zorgt dat je strings aan elkaar concatineert , voeg er iets toe aan achter.
+
+    }
+
+    while(1)
+    {
+        /// input a number to guess
+        ///Concatenate
+        std::stringstream id;
+        id << "BC!>input a " << nummer << "-digit number : ";
+        std:: string s = id.str();
+        rs = zmq_send (sender , s.c_str(),  s.size() , 0);
+        sleep(1);
+
+
+        std::string gs= "input";
+        rr = zmq_recv (receiver, buf ,256 , 0 ); ////receiving message option 1
+        /// wenselijk heb ik de volgende string ontvangen BC?>1111>  --> ik heb nodig 1111
+        gs = buf;
+        buf[rr] = '\0'; ///String stop
+        printf("Received: %s" , buf);
+
+        //String splitting
+        std :: string strGuess = buf; // numbers in buff
+        std :: size_t posPart1 = strGuess.find ( ">");
+        std :: string Part1= strGuess.substr(0, posPart1);
+        std :: string Part1After = strGuess.substr ( posPart1 + 1);
+
+
+        std :: size_t posPart2 = Part1After.find(">");
+        std :: string Part2= Part1After.substr(0, posPart2);
+        std :: string Part2After = Part1After.substr(posPart2 +1);
+
+        std:: stringstream np; ///number part
+        np << '\n' << Part2 << '\n' ;
+        std:: string p = np.str();
+        rs = zmq_send (sender , p.c_str() , p.size () , 0);
+
+       if (!cin)/// (Guesses[size] < 1000 || Guesses[size] > 9999) check if input is int
+        {
+            cin.clear();
+            cin.ignore(256,  '\n');
+
+            std::stringstream fd;
+            fd << "BC!>!!! " << nummer << "-Digits bruh pls !!!\n";
+            std:: string f = fd.str();
+            rs = zmq_send (sender , f.c_str(),  f.size() , 0);
+            continue;
+        }
+        else
+        {
+        int bulls = 0;
+        int cows = 0;
+
+        std :: string Part2Copy = Part2 ;
+        std :: string resultCopy= result;
+
+
+
+        hint[3] = result [3];
+        cout << "hint gegeven" << hint << endl;
+        rc = zmq_send (sender , "hint" ,  30 , 0);
+
+        std::stringstream ht;
+        ht<<"hint!"<<hint<<"given hint";
+        std:: string h = ht.str();
+        rc = zmq_send (sender , h.c_str(),  h.size() , 0);
+
+
+
+
+
+        for (int i = 0; i < nummer; i++) ///loop cows
+        {
+            if (resultCopy[i] == Part2Copy[i])
+            {    /// if the same
+                cows++;
+                resultCopy[i] = -1;
+                Part2Copy[i] = -1;
+            }
+        }
+
+        for (int i = 0; i < nummer; i++) ///loop bulls
+        {
+            if ( Part2Copy [i] >= 0 )
+            {
+              for (int j = 0; j < nummer; j++) /// loop eelk nummer
+              {
+
+                 if ( Part2Copy[j] >= 0 && resultCopy[j] == Part2Copy[i])
+                 {    /// if the same
+                    bulls++;
+                    resultCopy[j] = -1;
+                    Part2Copy[i] = -1;
+                    break;
+                }
+             }
+          }
+
+        }
+        /// if 4 cows = RIGHT
+        if (cows == 4) {
+            size++;
+            break;
+        }
+        /// weergeven van de aantallen
+
+        std::stringstream bc; ///bullscows
+        bc << "BC!>" << bulls << " bulls\n";
+        bc << "BC!>" << cows << " cows\n";
+        std:: string b = bc.str();
+        rs = zmq_send (sender , b.c_str(),  b.size() , 0);
+
+        size++;
+        }
+    }
+
+    std::stringstream fi; ///finale
+
+    fi << "BC!>" "---\n";
+    fi << "You guessed it right\n";
+    fi << "The correct number is " << result << endl;;
+    fi<< "These are all the numbers you'd guessed.\n";
+    std:: string f = fi.str();
+    rs = zmq_send (sender , f.c_str(),  f.size() , 0);
+
+    for (int i = 0; i < size; i++)
+    {
+        std::stringstream gu;
+        gu << "BC!>" << Guesses[i] << endl;
+        std:: string g = gu.str();
+        rs = zmq_send (sender , g.c_str(),  g.size() , 0);
+    }
+    rs = zmq_send (sender , "BullsCows Stopped" ,  17 , 0);
+    return 0;
+}
+ }
